0ce26bc4ad46aaaa861db21251e917fce9ee7c33
vexra <112993677+vexra@users.noreply.github.com>
2025-06-18 05:17:39 -0400

    fix: fix error

--- database/migrations/2025_05_25_135749_create_pesanan_table.php
+++ database/migrations/2025_05_25_135749_create_pesanan_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
 {
     Schema::create('pesanan', function (Blueprint $table) {
         $table->id();
-        $table->foreignId('pelanggan_id')->constrained()->onDelete('cascade');
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
         $table->string('rute');
         $table->decimal('harga', 10, 2);
         $table->string('maskapai');
